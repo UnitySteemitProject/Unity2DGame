bfd451bd901b87bc9ee656385e16a01bf5c93901
Jacob <wnsl5684@gmail.com>
2018-06-11 01:13:09 +0900

    Now, I am watching video. https://www.youtube.com/watch?v=VAyWk0eIqRc&t=11s
    I am learning 2d sprite animation.

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+
+    Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+        anim = GetComponent<Animator>();
+        anim.SetInteger("State", 0);
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(Input.GetKeyDown(KeyCode.W))
+            anim.SetInteger("State", 1);
+        else if(Input.GetKeyUp(KeyCode.W))
+            anim.SetInteger("State", 0);
+
+        if (Input.GetKey(KeyCode.I))
+            anim.SetInteger("State", 0);
+        else if (Input.GetKey(KeyCode.R))
+            anim.SetInteger("State", 2);
+    }
+}
